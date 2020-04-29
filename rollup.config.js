d3d8d362a2500e5cec5a1eec925ea8b752a7738a
Doğan Öztürk <doganozturk2005@gmail.com>
2020-04-29 18:52:55 +0300

    Initial project structure & linter & formatter setups & Rollup configuration.

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+        },
+        {
+            file: pkg.browser,
+            format: 'iife',
+            name: 'MetricsCatalog',
+        },
+    ],
+    external: [...Object.keys(pkg.dependencies || {})],
+    plugins: [
+        typescript({
+            typescript: require('typescript'),
+        }),
+        terser(),
+    ],
+};
